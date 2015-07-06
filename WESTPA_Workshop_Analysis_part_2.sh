217948846ecdf7cbb81308ab1cba636eef229f02
ASinanSaglam <asinansaglam@gmail.com>
2015-07-06 14:46:54 -0400

    Added a comment, testing the new git config

--- WESTPA_Workshop_Analysis_part_2.sh
+++ WESTPA_Workshop_Analysis_part_2.sh
@@ -48,6 +48,10 @@ w_kinavg trace -W ../west.h5 -a assign.h5 -k kinetics.h5 -o kinavg.h5 | tee kina
 
 
 # Now defining the state from arbitrary data sets
+# The following commands reassign the data, including the end to end distance into bins
+# as defined in BINS_AUX and with states as defined in STATES_AUX which is then
+# used to calculate rate constants.
+
 w_assign -W ../west.h5 --bins-from-file BINS_AUX --states-from-file STATES_AUX -o assign_aux.h5 --construct-dataset assignment.pull_data
 
 w_kinetics trace -W ../west.h5 -a assign_aux.h5 -o kinetics_aux.h5
