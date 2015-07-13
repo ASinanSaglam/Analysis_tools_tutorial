528ead86ce8ad3e0aa14d24014d7d2bd4956a9b8
ASinanSaglam <asinansaglam@gmail.com>
2015-07-13 16:59:29 -0400

    Added environment loader

---
+++ load_env.sh
@@ -0,0 +1,7 @@
+# Created by: Ali Sinan Saglam
+# For 2015 WESTPA Workshop, analysis tools 
+
+module purge
+module load queue
+module load westpa/1.0-gcc-4.8.2
+module load westpa/anaconda-1.9.1-gcc-4.8.2
